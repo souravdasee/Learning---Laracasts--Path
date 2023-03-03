fe732586fb27d5ab61036dd56ed45cb1372ee1e4
Sourav Das <souravdas4816@gmail.com>
2023-03-03 21:42:56 +0530

    +

--- 01 - PHP For Beginners/03 - Notes Mini-Project/23 - Extract a Simple Validator Class/Validator.php
+++ 01 - PHP For Beginners/03 - Notes Mini-Project/23 - Extract a Simple Validator Class/Validator.php
@@ -9,8 +9,8 @@ class Validator
         return strlen($value) >= $min && strlen($value) <= $max;
     }
 
-    // public static function email($value)
-    // {
-    //     return filter_var($value, FILTER_VALIDATE_EMAIL);
-    // }
+/*  public static function email($value)
+    {
+        return filter_var($value, FILTER_VALIDATE_EMAIL);
+    }   */
 }
