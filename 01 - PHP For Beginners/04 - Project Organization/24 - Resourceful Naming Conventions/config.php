8bf4715b98c0429bb845bfca71552a850a8d1ea7
Sourav Das <souravdas4816@gmail.com>
2023-03-04 16:44:23 +0530

    Learning from laracasts.com
    Taught by Jeff. Way

---
+++ 01 - PHP For Beginners/04 - Project Organization/24 - Resourceful Naming Conventions/config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'database' => 
+    [
+        'host' => 'localhost',
+        'port' => 3306,
+        'dbname' => 'myapp',
+        'charset' => 'utf8mb4'
+    ]
+
+/*  'services' => [
+        'prerednder' => [
+            'token' => '',
+            'secret' => ''
+        ]
+    ]   */
+];
