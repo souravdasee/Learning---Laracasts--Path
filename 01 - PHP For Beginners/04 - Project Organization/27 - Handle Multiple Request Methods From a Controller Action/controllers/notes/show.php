7634f0c8068b6217ec3819d9cdbbbbdc867185fb
Sourav Das <souravdas4816@gmail.com>
2023-03-06 11:37:46 +0530

    32

---
+++ 01 - PHP For Beginners/04 - Project Organization/27 - Handle Multiple Request Methods From a Controller Action/controllers/notes/show.php
@@ -0,0 +1,36 @@
+<?php
+
+use Core\Database;
+
+$config = require base_path('config.php');
+
+$db = new Database($config['database']);
+
+$currentUserId = 1;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+    
+    authorize($note['user_id'] === $currentUserId);
+
+    // form was submitted. delete the current note.
+    $db->query('delete from notes where id = :id', [
+        'id' => $_GET['id']
+    ]);
+
+    header('location: /notes');
+
+} else {
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    view("notes/show.view.php", [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+}
---
+++ 01 - PHP For Beginners/04 - Project Organization/27 - Handle Multiple Request Methods From a Controller Action/public/index.php
@@ -0,0 +1,24 @@
+<?php 
+
+const BASE_PATH = __DIR__ . '/../';
+
+
+require BASE_PATH . 'Core/functions.php';
+
+spl_autoload_register(function ($class)
+{
+    // Core\Database
+    $class = str_replace('\\', '/', $class);
+
+    require base_path("{$class}.php");
+});
+
+require base_path('Core/router.php');
+
+
+// $id = $_GET['id'];
+// $query = "select * from posts where id = :id";  //we can use "?" in ":id"
+
+// $posts = $db->query($query, ['id' => $id])->fetch();  // then 'id' will not be an associative array, i.e. only "$id"  &&  // we can use ':id' in 'id' associative
+
+// dd($posts);
