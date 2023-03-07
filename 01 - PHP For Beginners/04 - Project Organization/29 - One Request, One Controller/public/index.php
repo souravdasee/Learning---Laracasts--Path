622dda0771f17891cd12c1caa9468205c393f51d
Sourav Das <souravdas4816@gmail.com>
2023-03-07 13:57:51 +0530

    34

---
+++ 01 - PHP For Beginners/04 - Project Organization/29 - One Request, One Controller/public/index.php
@@ -0,0 +1,31 @@
+<?php 
+
+const BASE_PATH = __DIR__ . '/../';
+
+
+require BASE_PATH . 'Core/functions.php';
+
+spl_autoload_register(function ($class)
+{
+    // Core\Database
+    $class = str_replace('\\', '/', $class);
+
+    require base_path("{$class}.php");
+});
+
+$router = new \Core\Router();
+
+$routes = require base_path('routes.php');
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method);
+
+
+// $id = $_GET['id'];
+// $query = "select * from posts where id = :id";  //we can use "?" in ":id"
+
+// $posts = $db->query($query, ['id' => $id])->fetch();  // then 'id' will not be an associative array, i.e. only "$id"  &&  // we can use ':id' in 'id' associative
+
+// dd($posts);
