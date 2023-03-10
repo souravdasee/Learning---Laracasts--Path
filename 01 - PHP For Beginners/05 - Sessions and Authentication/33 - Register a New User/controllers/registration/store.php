42cf09fa0ded5928b90e2172b0ca2a2036a186f5
Sourav Das <souravdas4816@gmail.com>
2023-03-10 17:59:51 +0530

    Update store.php

--- 01 - PHP For Beginners/05 - Sessions and Authentication/33 - Register a New User/controllers/registration/store.php
+++ 01 - PHP For Beginners/05 - Sessions and Authentication/33 - Register a New User/controllers/registration/store.php
@@ -10,6 +10,8 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 
 $errors = [];
+
+// Validate the form inputs
 if (!Validator::email($email)) {
    $errors['email'] = 'Please provide a valid email address.';
 }
@@ -24,23 +26,29 @@ if (! empty($errors)) {
     ]);
 }
 
+// Check if the account already exists
 $user = $db->query('select * from users where email = :email', [
     'email' => $email
 ])->find();
 
 if ($user) {
+    // then someone with that email already exists and has an account
+    // if yes, redirect to a login page.
     header('location: /');
     exit();
 } else {
+    // if not, save one to the database, and then log the user in, and redirect.
     $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
         'email' => $email,
         'password' => $password // NEVER store database passwords in clear text. We'll fix this in the login form episode. :)
     ]);
 
+    // mark that the user has logged in.
     $_SESSION['user'] = [
         'email' => $email
     ];
 
+    // after confirming logged-in redirect to homepage.
     header('location: /');
     exit();
 }
