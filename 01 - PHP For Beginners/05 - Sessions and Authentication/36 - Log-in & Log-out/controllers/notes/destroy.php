5de1f3274f62c0e1f04b1451a980e1c4a8ec2a88
Sourav Das <85134017+souravdasee@users.noreply.github.com>
2023-03-17 08:07:08 +0530

    41 - Login & Logout

---
+++ 01 - PHP For Beginners/05 - Sessions and Authentication/36 - Log-in & Log-out/controllers/notes/destroy.php
@@ -0,0 +1,23 @@
+<?php
+
+use Core\App;
+use Core\Database;
+
+$db = App::resolve(Database::class);
+
+
+$currentUserId = 1;
+
+$note = $db->query('select * from notes where id = :id', [
+    'id' => $_POST['id']
+])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+
+// form was submitted. delete the current note.
+$db->query('delete from notes where id = :id', [
+    'id' => $_POST['id']
+]);
+
+header('location: /notes');
+exit();
---
+++ 01 - PHP For Beginners/05 - Sessions and Authentication/36 - Log-in & Log-out/controllers/session/store.php
@@ -0,0 +1,50 @@
+<?php
+
+use Core\App;
+use Core\Database;
+use Core\Validator;
+
+$db = App::resolve(Database::class);
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$errors = [];
+
+// Validate the form inputs
+if (!Validator::email($email)) {
+   $errors['email'] = 'Please provide a valid email address.';
+}
+
+if (!Validator::string($password)) {
+    $errors['password'] = 'Please provide a valid password.';
+}
+
+if (! empty($errors)) {
+    return view('session/create.view.php', [
+        'errors' => $errors
+    ]);
+}
+
+// match the credentials
+$user = $db->query('select * from users where email = :email', [
+    'email' => $email
+])->find();
+
+if($user) {
+// we have a user, but we don not know if the password provided matches what we have in the database.
+    if (password_verify($password, $user['password'])) {
+        login([
+            'email' => $email
+        ]);
+
+        header('location: /');
+        exit();
+    }
+}
+
+return view('session/create.view.php', [
+    'errors' => [
+        'email' => 'No matching account found for that email address and password.'
+    ]
+]);
