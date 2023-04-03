728fc84ed58fdd4f60629c20e657917390adbb35
Sourav Das <developer.souravdas.ee@gmail.com>
2023-04-03 21:01:41 +0530

    PHP-OOPS

---
+++ 02 - Object Oriented Programming in PHP/01 - Classes.php
@@ -0,0 +1,23 @@
+<?php
+
+class Invoice
+{
+
+}
+
+class InvoiceItem
+{
+
+}
+
+class AchievementBadge
+{
+    public $title;
+    public $description;
+    public $points;
+
+    public function awardTo($user)
+    {
+        //
+    }
+}
---
+++ 02 - Object Oriented Programming in PHP/02 - Objects.php
@@ -0,0 +1,70 @@
+<?php
+
+class Team
+{
+    protected $name;
+    protected $members = [];
+
+    public function __construct($name, $members = [])
+    {
+        $this->name = $name;
+        $this->members = $members;
+    }
+
+    public static function start(...$params)
+    {
+        return new static(...$params);
+    }
+
+    public function name()
+    {
+        return $this->name;
+    }
+
+    public function members()
+    {
+        return $this->members;
+    }
+
+    public function add($name)
+    {
+        $this->members[] = $name;
+    }
+
+    public function cancel()
+    {
+
+    }
+
+    public function manager()
+    {
+
+    }
+}
+
+class Member
+{
+    protected $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function lastViewed()
+    {
+
+    }
+}
+
+$acme = Team::start('Acme', [
+    new Member('John Doe'),
+    new Member('Jane Doe')
+]);
+
+$acme->add('Frank Doe');
+
+$laracasts = new Team('Laracasts');
+$laracasts = new Team('Foo');
+
+var_dump($acme->members());
