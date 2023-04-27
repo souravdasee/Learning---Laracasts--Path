a2f89c922a8bc5e1c904312b5da092cb4d6dc1df
Sourav Das <developer.souravdas.ee@gmail.com>
2023-04-28 03:11:19 +0530

    initial commit - Composer and Free Autoloading

---
+++ 01 - PHP For Beginners/07 - Meet Composer/42-Composer_and_Free_Autoloading/Core/Authenticator.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Core;
+
+class Authenticator
+{
+    public function attempt($email, $password)
+    {
+        // match the credentials
+        $user = App::resolve(Database::class)
+        ->query('select * from users where email = :email', [
+            'email' => $email
+            ])->find();
+
+        if ($user) {
+        // we have a user, but we don not know if the password provided matches what we have in the database.
+            if (password_verify($password, $user['password'])) {
+                $this->login([
+                    'email' => $email
+                ]);
+
+                return true;
+            }
+        }
+
+    return false;
+    }
+
+    public function login($user)
+    {
+        $_SESSION['user'] = [
+            'email' => $user['email']
+        ];
+
+        session_regenerate_id(true);
+    }
+
+    public function logout() 
+    {
+        Session::destroy();
+    }
+}
