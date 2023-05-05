45dc3cd43da4628e0a2a770155df74f353d95182
Sourav Das <developer.souravdas.ee@gmail.com>
2023-05-05 13:34:10 +0530

    Store Blog Posts as HTML Files

--- 11 - Laravel 8 from Scratch/blog/resources/views/posts.blade.php
+++ 11 - Laravel 8 from Scratch/blog/resources/views/posts.blade.php
@@ -5,7 +5,7 @@
 
 <body>
     <article>
-        <h1><a href="/post">My First Post</a></h1>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
 
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et quisquam libero delectus voluptatem ut numquam, atque dolore? Quam veritatis facere optio, doloremque rerum, dolorem vel maiores vitae consectetur deserunt ullam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quam laboriosam repellendus consequatur tenetur dolore vel nisi assumenda laborum neque dolorum eos eveniet ipsam, velit corporis animi molestiae nostrum optio? Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt incidunt consectetur, fuga accusantium, praesentium, nihil consequatur in facilis id maxime impedit! Ea voluptate nulla nostrum dolor illum nisi, quidem libero.
@@ -13,7 +13,7 @@
     </article>
 
     <article>
-        <h1><a href="/post">My Second Post</a></h1>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
 
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et quisquam libero delectus voluptatem ut numquam, atque dolore? Quam veritatis facere optio, doloremque rerum, dolorem vel maiores vitae consectetur deserunt ullam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quam laboriosam repellendus consequatur tenetur dolore vel nisi assumenda laborum neque dolorum eos eveniet ipsam, velit corporis animi molestiae nostrum optio? Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt incidunt consectetur, fuga accusantium, praesentium, nihil consequatur in facilis id maxime impedit! Ea voluptate nulla nostrum dolor illum nisi, quidem libero.
@@ -21,7 +21,7 @@
     </article>
 
     <article>
-        <h1><a href="/post">My Third Post</a></h1>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
 
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et quisquam libero delectus voluptatem ut numquam, atque dolore? Quam veritatis facere optio, doloremque rerum, dolorem vel maiores vitae consectetur deserunt ullam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quam laboriosam repellendus consequatur tenetur dolore vel nisi assumenda laborum neque dolorum eos eveniet ipsam, velit corporis animi molestiae nostrum optio? Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt incidunt consectetur, fuga accusantium, praesentium, nihil consequatur in facilis id maxime impedit! Ea voluptate nulla nostrum dolor illum nisi, quidem libero.
