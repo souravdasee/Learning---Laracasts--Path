7e836decb6e6eae7f4b7e4b857d38b81e94def80
Sourav Das <developer.souravdas.ee@gmail.com>
2023-06-08 18:23:43 +0530

    Turbo Boost With Factories

--- 11 - Laravel 8 from Scratch/blog/database/seeders/DatabaseSeeder.php
+++ 11 - Laravel 8 from Scratch/blog/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -14,54 +13,14 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run()
+    public function run(): void
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal',
-        ]);
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family',
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work',
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'slug' => 'my-first-post',
-            'title' => 'My Personal Post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et quisquam libero delectus voluptatem ut numquam, atque dolore? Quam veritatis facere optio, doloremque rerum, dolorem vel maiores vitae consectetur deserunt ullam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quam laboriosam repellendus consequatur tenetur dolore vel nisi assumenda laborum neque dolorum eos eveniet ipsam, velit corporis animi molestiae nostrum optio? Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt incidunt consectetur, fuga accusantium, praesentium, nihil consequatur in facilis id maxime impedit! Ea voluptate nulla nostrum dolor illum nisi, quidem libero.</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'slug' => 'my-second-post',
-            'title' => 'My Family Post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et quisquam libero delectus voluptatem ut numquam, atque dolore? Quam veritatis facere optio, doloremque rerum, dolorem vel maiores vitae consectetur deserunt ullam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quam laboriosam repellendus consequatur tenetur dolore vel nisi assumenda laborum neque dolorum eos eveniet ipsam, velit corporis animi molestiae nostrum optio? Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt incidunt consectetur, fuga accusantium, praesentium, nihil consequatur in facilis id maxime impedit! Ea voluptate nulla nostrum dolor illum nisi, quidem libero.</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'slug' => 'my-third-post',
-            'title' => 'My Work Post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et quisquam libero delectus voluptatem ut numquam, atque dolore? Quam veritatis facere optio, doloremque rerum, dolorem vel maiores vitae consectetur deserunt ullam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quam laboriosam repellendus consequatur tenetur dolore vel nisi assumenda laborum neque dolorum eos eveniet ipsam, velit corporis animi molestiae nostrum optio? Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt incidunt consectetur, fuga accusantium, praesentium, nihil consequatur in facilis id maxime impedit! Ea voluptate nulla nostrum dolor illum nisi, quidem libero.</p>'
-        ]);
-
     }
 }
