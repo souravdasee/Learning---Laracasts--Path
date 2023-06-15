f686812f031056cf61ef45b1ff683daeb2a79e0e
Sourav Das <souravdas4816@gmail.com>
2023-06-15 15:19:02 +0530

    Fix a Confusing Eloquent Query Bug

--- 11 - Laravel 8 from Scratch/blog/resources/views/components/category-dropdown.blade.php
+++ 11 - Laravel 8 from Scratch/blog/resources/views/components/category-dropdown.blade.php
@@ -9,14 +9,14 @@
 
     <x-dropdown-item
         href="/"
-        :active="request()->routeIs('home')" >
+        :active="request('category') === null" >
             All
     </x-dropdown-item>
 
     @foreach ($categories as $category)
         <x-dropdown-item
-            href="/?category={{ $category->slug }}"
-            :active='request()->is("categories/{$category->slug}")'>
+            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
+            :active="request('category') === $category->slug">
             {{ ucwords($category->name) }}
         </x-dropdown-item>
     @endforeach
