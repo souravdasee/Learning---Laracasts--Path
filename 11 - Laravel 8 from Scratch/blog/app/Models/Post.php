d5e26569335cef2d89cee529b87c55dcf1327bdd
Sourav Das <souravdas4816@gmail.com>
2023-06-15 10:20:58 +0530

    Search (The Cleaner Way)

--- 11 - Laravel 8 from Scratch/blog/app/Models/Post.php
+++ 11 - Laravel 8 from Scratch/blog/app/Models/Post.php
@@ -15,6 +15,23 @@ class Post extends Model
 
     protected $with = ['category', 'author'];
 
+
+
+
+
+    public function scopeFilter($query, array $filters) //Post::newQuery()->filter()
+    {
+        $query->when($filters['search'] ?? false, fn($query, $search) =>
+            $query
+                ->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%'));
+    }
+
+
+
+
+
+
     public function category ()
     {
         // hasOne, hasMany, belongsTo, belongsMany
