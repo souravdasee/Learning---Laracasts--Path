062d59ce427d68a504778f02cee64bfb14910720
Sourav Das <souravdas4816@gmail.com>
2023-06-15 13:30:40 +0530

    Author Filtering

--- 11 - Laravel 8 from Scratch/blog/app/Models/Post.php
+++ 11 - Laravel 8 from Scratch/blog/app/Models/Post.php
@@ -30,6 +30,11 @@ class Post extends Model
             $query->whereHas('category', fn ($query) =>
                 $query->where('slug', $category)
             ));
+
+        $query->when($filters['author'] ?? false, fn($query, $author) =>
+            $query->whereHas('author', fn ($query) =>
+                $query->where('username', $author)
+            ));
     }
 
 
--- 11 - Laravel 8 from Scratch/blog/routes/web.php
+++ 11 - Laravel 8 from Scratch/blog/routes/web.php
@@ -1,27 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Post;
-use App\Models\User;
-use App\Models\Category;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show'] );
-
-Route::get('authors/{author:username}', function(User $author) {
-    return view('posts', [
-        'posts' => $author->posts
-    ]);
-});
