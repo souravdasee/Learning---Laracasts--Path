40dc2d50d28f6fdff8add2ab393ed4afc5983139
Sourav Das <souravdas4816@gmail.com>
2023-06-17 12:54:30 +0530

    Build the Log In Page

--- 11 - Laravel 8 from Scratch/blog/app/Http/Controllers/SessionsController.php
+++ 11 - Laravel 8 from Scratch/blog/app/Http/Controllers/SessionsController.php
@@ -2,8 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        // validate the request
+        $attributes = request()->validate([
+            'email' => 'required | email',
+            'password' => 'required'
+        ]);
+
+        // attempt to authenticate and log in the user based on the provided credentials
+        if (auth()->attempt($attributes)) {
+            // session fixation
+            session()->regenerate();
+            
+            // redirect with a success flash message
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        // auth failed
+        /*
+        return back()
+            ->withInput()
+            ->withErrors(['email' => 'Your provided credentials could not be verified.']);
+        */
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified.'
+        ]);
+    }
+
     public function destroy()
     {
         auth()->logout();
