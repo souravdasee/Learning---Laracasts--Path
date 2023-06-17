dabcbb235330ec49973fa061ab35b18fd6563c58
Sourav Das <souravdas4816@gmail.com>
2023-06-17 16:38:44 +0530

    Design the Comment Form

--- 11 - Laravel 8 from Scratch/blog/resources/views/components/post-comment.blade.php
+++ 11 - Laravel 8 from Scratch/blog/resources/views/components/post-comment.blade.php
@@ -1,22 +1,24 @@
 @props(['comment'])
 
-<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
-    <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
-    </div>
+<x-panel class="bg-gray-50">
+    <article class="flex space-x-4">
+        <div class="flex-shrink-0">
+            <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
+        </div>
 
-    <div>
-        <header class="mb-4">
-            <h3 class="font-bold">{{ $comment->author->username }}</h3>
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->username }}</h3>
 
-            <p>
-                Posted
-                <time>{{ $comment->created_at }}</time>
-            </p>
+                <p>
+                    Posted
+                    <time>{{ $comment->created_at }}</time>
+                </p>
 
-            <p>
-                {{ $comment->body }}
-            </p>
-        </header>
-    </div>
-</article>
+                <p>
+                    {{ $comment->body }}
+                </p>
+            </header>
+        </div>
+    </article>
+</x-panel>
