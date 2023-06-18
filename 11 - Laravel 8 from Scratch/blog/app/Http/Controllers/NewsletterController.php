336d74bc2330cb05d0f122dae7471208ab5fbfcb
Sourav Das <souravdas4816@gmail.com>
2023-06-18 15:26:59 +0530

    Automatic resolution, Containers, Service providers & Contracts

--- 11 - Laravel 8 from Scratch/blog/app/Http/Controllers/NewsletterController.php
+++ 11 - Laravel 8 from Scratch/blog/app/Http/Controllers/NewsletterController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Services\Newsletter;
-use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 
 class NewsletterController extends Controller
--- 11 - Laravel 8 from Scratch/blog/app/Providers/AppServiceProvider.php
+++ 11 - Laravel 8 from Scratch/blog/app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use App\Services\Newsletter;
+use App\Services\MailchimpNewsletter;
+use MailchimpMarketing\ApiClient;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,7 +15,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        app()->bind(Newsletter::class, function () {
+            $client = (new ApiClient)->setConfig([
+                'apiKey' => config('services.mailchimp.key'),
+                'server' => 'us21'
+            ]);
+
+            return new MailchimpNewsletter($client);
+        });
     }
 
     /**
