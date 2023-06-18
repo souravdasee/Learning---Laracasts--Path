8ccc07576e2e8547fa3f7fbdbe491fcfe2ded094
Sourav Das <souravdas4816@gmail.com>
2023-06-18 13:41:49 +0530

    Extract a Newsletter Service

--- 11 - Laravel 8 from Scratch/blog/routes/web.php
+++ 11 - Laravel 8 from Scratch/blog/routes/web.php
@@ -1,42 +1,19 @@
 <?php
 
+use App\Services\Newsletter;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
-
-Route::post('newsletter', function () {
-    request()->validate(['email' => 'required | email']);
-
-    $mailchimp = new \MailchimpMarketing\ApiClient();
-
-    $mailchimp->setConfig([
-        'apiKey' => config('services.mailchimp.key'),
-        'server' => 'us21'
-    ]);
-
-    try {
-        $response = $mailchimp->lists->addListMember(
-            "aec6858cb0",
-            [
-                "email_address" => request('email'),
-                "status" => "subscribed",
-            ]
-        );
-    } catch (\Exception $e) {
-        throw \Illuminate\Validation\ValidationException::withMessages([
-            'email' => 'This email could not be added to our newsletter list.'
-        ]);
-    }
-
-    return redirect('/')->with('success', 'You are now signed up for our newsletter!');
-});
+use App\Http\Controllers\NewsletterController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
-Route::get('posts/{post:slug}', [PostController::class, 'show'] );
 
+Route::get('posts/{post:slug}', [PostController::class, 'show'] );
 Route::post('posts/{post:slug}/comments', [CommentController::class, 'store']);
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
