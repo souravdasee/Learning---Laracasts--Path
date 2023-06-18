451ac3f7fcdf85249d9b2c250c115244da877c28
Sourav Das <souravdas4816@gmail.com>
2023-06-18 12:19:13 +0530

    Mailchimp API Tinkering

--- 11 - Laravel 8 from Scratch/blog/routes/web.php
+++ 11 - Laravel 8 from Scratch/blog/routes/web.php
@@ -6,6 +6,22 @@ use App\Http\Controllers\CommentController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us21'
+    ]);
+
+    $response = $mailchimp->lists->addListMember("aec6858cb0",
+    [
+        "email_address" => "meteoricmarvel@gmail.com",
+        "status" => "subscribed",
+    ]);
+    ddd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show'] );
 
