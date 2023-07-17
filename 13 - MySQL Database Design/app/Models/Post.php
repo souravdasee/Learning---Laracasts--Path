8ddda22cffd81f80f1108f6c5600ab67654e4c06
Sourav Das <souravdas4816@gmail.com>
2023-07-17 18:16:54 +0530

    Many-to-Many Relations

--- 13 - MySQL Database Design/app/Models/Post.php
+++ 13 - MySQL Database Design/app/Models/Post.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Tag;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
@@ -12,5 +13,10 @@ class Post extends Model
     public function author()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
     }
 }
